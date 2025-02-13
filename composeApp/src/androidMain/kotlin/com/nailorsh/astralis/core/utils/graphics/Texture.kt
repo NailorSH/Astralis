90a93c1d484ab3fd4f3ce323b40386deffbe3d4b
Nail Shiyatov <nailshiyatov.04@mail.ru>
2025-02-13 11:11:47 +0300

    refactor(graphics): update loadTexture

--- composeApp/src/androidMain/kotlin/com/nailorsh/astralis/core/utils/graphics/Texture.kt
+++ composeApp/src/androidMain/kotlin/com/nailorsh/astralis/core/utils/graphics/Texture.kt
@@ -2,45 +2,59 @@ package com.nailorsh.astralis.core.utils.graphics
 
 import android.content.Context
 import android.graphics.BitmapFactory
-import android.opengl.GLES20
 import android.opengl.GLUtils
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.ImageBitmapConfig
+import com.danielgergely.kgl.GL_ALPHA
+import com.danielgergely.kgl.GL_CLAMP_TO_EDGE
+import com.danielgergely.kgl.GL_LINEAR
+import com.danielgergely.kgl.GL_RGB
+import com.danielgergely.kgl.GL_RGBA
+import com.danielgergely.kgl.GL_TEXTURE_2D
+import com.danielgergely.kgl.GL_TEXTURE_MAG_FILTER
+import com.danielgergely.kgl.GL_TEXTURE_MIN_FILTER
+import com.danielgergely.kgl.GL_TEXTURE_WRAP_S
+import com.danielgergely.kgl.GL_TEXTURE_WRAP_T
+import com.danielgergely.kgl.GL_UNSIGNED_BYTE
+import com.danielgergely.kgl.GL_UNSIGNED_SHORT_5_6_5
+import com.danielgergely.kgl.KglAndroid.bindTexture
+import com.danielgergely.kgl.KglAndroid.createTexture
+import com.danielgergely.kgl.KglAndroid.texParameteri
 
 fun loadTexture(context: Context, resourceId: Int): Int {
-    val textureHandle = IntArray(1)
-    GLES20.glGenTextures(1, textureHandle, 0)
+    val textureHandle = createTexture()
 
-    if (textureHandle[0] != 0) {
+    if (textureHandle != 0) {
+        bindTexture(GL_TEXTURE_2D, textureHandle)
+
+        texParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+        texParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+        texParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE)
+        texParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE)
+
         val bitmap = BitmapFactory.decodeResource(context.resources, resourceId)
-        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textureHandle[0])
+        GLUtils.texImage2D(GL_TEXTURE_2D, 0, bitmap, 0)
+        bitmap.recycle()
 
-        // Устанавливаем параметры текстуры
-        GLES20.glTexParameteri(
-            GLES20.GL_TEXTURE_2D,
-            GLES20.GL_TEXTURE_MIN_FILTER,
-            GLES20.GL_LINEAR
-        )
-        GLES20.glTexParameteri(
-            GLES20.GL_TEXTURE_2D,
-            GLES20.GL_TEXTURE_MAG_FILTER,
-            GLES20.GL_LINEAR
-        )
-        GLES20.glTexParameteri(
-            GLES20.GL_TEXTURE_2D,
-            GLES20.GL_TEXTURE_WRAP_S,
-            GLES20.GL_CLAMP_TO_EDGE
-        )
-        GLES20.glTexParameteri(
-            GLES20.GL_TEXTURE_2D,
-            GLES20.GL_TEXTURE_WRAP_T,
-            GLES20.GL_CLAMP_TO_EDGE
-        )
-
-        // Загружаем изображение в текстуру
-        GLUtils.texImage2D(GLES20.GL_TEXTURE_2D, 0, bitmap, 0)
-        bitmap.recycle() // Освобождаем ресурсы
-
-        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, 0) // Отвязываем текстуру
+        bindTexture(GL_TEXTURE_2D, null)
     }
 
-    return textureHandle[0]
+    return textureHandle
+}
+
+fun getInternalFormat(imageBitmap: ImageBitmap): Int {
+    return when (imageBitmap.config) {
+        ImageBitmapConfig.Alpha8 -> GL_ALPHA
+        ImageBitmapConfig.Argb8888 -> GL_RGBA
+        ImageBitmapConfig.Rgb565 -> GL_RGB
+        else -> throw IllegalArgumentException("Unknown internalformat")
+    }
+}
+
+fun getType(imageBitmap: ImageBitmap): Int {
+    return when (imageBitmap.config) {
+        ImageBitmapConfig.Rgb565 -> GL_UNSIGNED_SHORT_5_6_5
+        ImageBitmapConfig.Alpha8, ImageBitmapConfig.Argb8888 -> GL_UNSIGNED_BYTE
+        else -> GL_UNSIGNED_BYTE
+    }
 }
