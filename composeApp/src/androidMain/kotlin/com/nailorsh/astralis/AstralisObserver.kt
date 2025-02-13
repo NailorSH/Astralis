3022bd82d59806bddcf88bcef2712a64ba34e992
Nail Shiyatov <nailshiyatov.04@mail.ru>
2025-02-13 08:00:55 +0300

    backup commit

--- composeApp/src/androidMain/kotlin/com/nailorsh/astralis/AstralisObserver.kt
+++ composeApp/src/androidMain/kotlin/com/nailorsh/astralis/AstralisObserver.kt
@@ -1,22 +1,22 @@
-package com.nailorsh.astralis
-
-import com.nailorsh.astralis.core.utils.graphic.math.matrix.Mat4d
-import kotlin.math.PI
-
-abstract class AstralisObserver(loc: AstralisLocation) {
-    private val ssystem: SolarSystem = SolarSystem.getInstance()
-
-    private var currentLocation: AstralisLocation = loc
-    private var planet: Planet = ssystem.searchByEnglishName(loc.planetName) ?: ssystem.getEarth()
-
-    // Для Земли требуется JD, для других планет — JDE для описания вращения!
-    fun getRotAltAzToEquatorial(JD: Double, JDE: Double): Mat4d {
-        val lat = currentLocation.latitude.toDouble().coerceIn(-90.0, 90.0)
-
-        return Mat4d.zRotation(
-            (planet.getSiderealTime(JD, JDE) + currentLocation.longitude) *
-                    Math.PI / 180.0
-        ) * Mat4d.yRotation((90.0 - lat) * PI / 180.0)
-    }
-
-}
+//package com.nailorsh.astralis
+//
+//import com.nailorsh.astralis.core.utils.graphic.math.matrix.Mat4d
+//import kotlin.math.PI
+//
+//abstract class AstralisObserver(loc: AstralisLocation) {
+//    private val ssystem: SolarSystem = SolarSystem.getInstance()
+//
+//    private var currentLocation: AstralisLocation = loc
+//    private var planet: Planet = ssystem.searchByEnglishName(loc.planetName) ?: ssystem.getEarth()
+//
+//    // Для Земли требуется JD, для других планет — JDE для описания вращения!
+//    fun getRotAltAzToEquatorial(JD: Double, JDE: Double): Mat4d {
+//        val lat = currentLocation.latitude.toDouble().coerceIn(-90.0, 90.0)
+//
+//        return Mat4d.zRotation(
+//            (planet.getSiderealTime(JD, JDE) + currentLocation.longitude) *
+//                    Math.PI / 180.0
+//        ) * Mat4d.yRotation((90.0 - lat) * PI / 180.0)
+//    }
+//
+//}
--- composeApp/src/commonMain/kotlin/com/nailorsh/astralis/core/utils/graphic/EphemWrapper.kt
+++ composeApp/src/commonMain/kotlin/com/nailorsh/astralis/core/utils/graphic/EphemWrapper.kt
@@ -1,37 +1,37 @@
-package com.nailorsh.astralis.core.utils.graphic
-
-fun init_de430(filepath: String) {
-    TODO()
-}
-
-fun init_de431(filepath: String) {
-    TODO()
-}
-
-fun jd_fits_de430(jd: Double): Boolean = jd > 2287184.5 && jd < 2688976.5
-
-//Correct limits found via jpl_get_double(). Limits hardcoded to avoid calls each time.
-//return !(jd < -3027215.5 || jd > 7930192.5);
-//This limits inside those where sun can jump between ecliptic of date and ecliptic2000.
-// We lose a month in -13000 and a few months in +17000, this should not matter.
-fun jd_fits_de431(jd: Double): Boolean = jd > -3027188.25 && jd < 7930056.87916
-
-fun use_de430(jd: Double): Boolean {}
-fun use_de431(jd: Double): Boolean {}
-fun init_de440(filepath: String) {
-    TODO()
-}
-
-fun init_de441(filepath: String) {
-    TODO()
-}
-
-fun jd_fits_de440(jd: Double): Boolean = jd > 2287184.5 && jd < 2688976.5
-
-//Correct limits found via jpl_get_double(). Limits hardcoded to avoid calls each time.
-//return !(jd < -3027215.5 || jd > 7930192.5);
-//This limits inside those where sun can jump between ecliptic of date and ecliptic2000.
-// We lose a month in -13000 and a few months in +17000, this should not matter.
-fun jd_fits_de441(jd: Double): Boolean = jd > -3027188.25 && jd < 7930056.87916
-fun use_de440(jd: Double): Boolean {}
-fun use_de441(jd: Double): Boolean {}
+//package com.nailorsh.astralis.core.utils.graphic
+//
+//fun init_de430(filepath: String) {
+//    TODO()
+//}
+//
+//fun init_de431(filepath: String) {
+//    TODO()
+//}
+//
+//fun jd_fits_de430(jd: Double): Boolean = jd > 2287184.5 && jd < 2688976.5
+//
+////Correct limits found via jpl_get_double(). Limits hardcoded to avoid calls each time.
+////return !(jd < -3027215.5 || jd > 7930192.5);
+////This limits inside those where sun can jump between ecliptic of date and ecliptic2000.
+//// We lose a month in -13000 and a few months in +17000, this should not matter.
+//fun jd_fits_de431(jd: Double): Boolean = jd > -3027188.25 && jd < 7930056.87916
+//
+//fun use_de430(jd: Double): Boolean {}
+//fun use_de431(jd: Double): Boolean {}
+//fun init_de440(filepath: String) {
+//    TODO()
+//}
+//
+//fun init_de441(filepath: String) {
+//    TODO()
+//}
+//
+//fun jd_fits_de440(jd: Double): Boolean = jd > 2287184.5 && jd < 2688976.5
+//
+////Correct limits found via jpl_get_double(). Limits hardcoded to avoid calls each time.
+////return !(jd < -3027215.5 || jd > 7930192.5);
+////This limits inside those where sun can jump between ecliptic of date and ecliptic2000.
+//// We lose a month in -13000 and a few months in +17000, this should not matter.
+//fun jd_fits_de441(jd: Double): Boolean = jd > -3027188.25 && jd < 7930056.87916
+//fun use_de440(jd: Double): Boolean {}
+//fun use_de441(jd: Double): Boolean {}
