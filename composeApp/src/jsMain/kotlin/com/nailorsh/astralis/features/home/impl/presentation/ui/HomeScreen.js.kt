e0410c9c843b76dc862ce0e5f3bbe7573494df39
Nail Shiyatov <nailshiyatov.04@mail.ru>
2025-02-13 20:12:34 +0300

    feat: implement basic HomeScreen and AstralisTexture

---
+++ composeApp/src/jsMain/kotlin/com/nailorsh/astralis/features/home/impl/presentation/ui/HomeScreen.js.kt
@@ -0,0 +1,13 @@
+package com.nailorsh.astralis.features.home.impl.presentation.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.nailorsh.astralis.features.home.impl.presentation.data.model.BodyWithPosition
+
+@Composable
+actual fun SpaceScreen(
+    planets: List<BodyWithPosition>,
+    modifier: Modifier
+) {
+    // TODO
+}
---
+++ composeApp/src/jvmMain/kotlin/com/nailorsh/astralis/core/graphics/AstralisTexture.jvm.kt
@@ -0,0 +1,7 @@
+package com.nailorsh.astralis.core.utils.graphics
+
+import androidx.compose.ui.graphics.ImageBitmap
+
+actual fun texImage2D(target: Int, level: Int, bitmap: ImageBitmap, border: Int) {
+    // TODO
+}
