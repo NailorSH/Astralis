3b3616bc6563a1e3f23a43990623349ab15e9ed1
Nail Shiyatov <nailshiyatov.04@mail.ru>
2025-02-19 10:23:25 +0300

    remove unused files

--- composeApp/src/androidMain/kotlin/com/nailorsh/astralis/projector/ModelViewTranform.kt
+++
@@ -1,7 +0,0 @@
-//package com.nailorsh.astralis.projector
-//
-//import com.danielgergely.kgl.Shader
-//import com.nailorsh.astralis.core.utils.graphics.math.matrix.Mat4d
-//import com.nailorsh.astralis.core.utils.graphics.math.vector.Vec3d
-//import com.nailorsh.astralis.core.utils.graphics.math.vector.Vec3f
-//
